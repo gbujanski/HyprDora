d73e8f885f2e9ed144a2895d8c07f400f32cd104
Grzegorz Bujański <grzegorz.bujanski@gmail.com>
2026-05-18 19:28:36 +0200

    v0.50: add option to reboot after script finishes

--- install.sh
+++ install.sh
@@ -23,5 +23,14 @@ export GUM_CHOOSE_HEADER="Choose packages to install:
 source install/all.sh
 source postinstall/all.sh
 
-log_info "Installation complete!
-You can continue with further configuration."
+log_info "Installation complete!"
+log_info "Reboot is highly recommended to ensure all changes take effect properly."
+
+reboot_answer=$(gum confirm "Reboot now?" --affirmative="Yes" --negative="No" --default="Yes" && echo 1 || echo 0)
+
+if [[ "$reboot_answer" -eq 1 ]]; then
+	log_info "Rebooting..."
+	sudo reboot
+else
+	log_info "Please remember to reboot your system as soon as possible to ensure all changes take effect properly."
+fi
